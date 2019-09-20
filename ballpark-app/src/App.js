4f4d2b5668a2458a633d851691fa444f557d1dbe
Brett Derham <brettderham@Robins-MacBook-Pro.local>
2019-09-20 14:11:58 -0700

    Add some basic forms and button.

--- ballpark-app/src/App.js
+++ ballpark-app/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import { Navbar, Jumbotron, Button } from 'react-bootstrap';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Navbar, Jumbotron, Button, Form, Col, Row } from 'react-bootstrap';
 // function App() {
 //   return (
 //     <div className="App">
@@ -29,7 +29,7 @@ class App extends Component {
     return (
       <div>
         <Jumbotron>
-            <h1>Welcome to React</h1>
+            <h1>Ballpark Bookie</h1>
             <p>
               <Button
                 bsStyle="success"
@@ -38,6 +38,34 @@ class App extends Component {
                 target="_blank">
                 View React Bootstrap Docs
               </Button>
+            </p>
+            <p>
+             <Form>
+                <Form.Row>
+                  <Form.Group as={Col} md="6" controlId="team1">
+                    <Form.Label>Team One</Form.Label>
+                    <Form.Control type="team" placeholder="Enter Team One" />
+                  </Form.Group>
+                  <Form.Group as={Col} md="6" controlId="team2">                    
+                    <Form.Label>Team Two</Form.Label>
+                    <Form.Control type="team" placeholder="Enter Team Two" />
+                  </Form.Group>
+                  
+                </Form.Row>
+              </Form>
+            </p>
+
+            <p> 
+            <div class="mx-auto">
+              <Button variant="primary" type="compare">
+                Compare
+              </Button>
+            </div>
+            <div className="mx-auto">
+              <Form.Text className="text-muted">
+                Team ____ will win the game.
+              </Form.Text>
+              </div>
             </p>
         </Jumbotron>
       </div>
