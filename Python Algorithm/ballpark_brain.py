eda591c3e3148a79868f6443ddce5789d4539844
kea <kea.braekman@mail.com>
2019-10-18 21:45:16 -0700

    added python file to test API

---
+++ Python Algorithm/ballpark_brain.py
@@ -0,0 +1,40 @@
+# Install the Python Requests library:
+# `pip install requests`
+
+import base64
+import requests
+
+season = '2018-2019-regular'
+apikey_token = '5cd1a799-8747-4eb4-8209-9f4850'
+# apikey_token = 'Xx_thomas_xX'
+password = 'ilikevike'
+# MYSPORTSFEED = 0
+for_date = '20150606'
+# pull_url = 'https://api.mysportsfeeds.com/v1.0/pull/mlb/current_season.json?fordate=' + for_date
+pull_url = 'https://api.mysportsfeeds.com/v1.2/pull/mlb/2015-2016-regular/cumulative_player_stats.json'
+
+# pull_url = 'https://api.mysportsfeeds.com/v1.0/pull/mlb/2015-2016-regular/daily_player_stats.json?fordate=' + for_date
+
+
+def send_request():
+    # Request
+
+    try:
+        response = requests.get(
+            url=pull_url,
+            params={
+                "fordate": "20161121"
+            },
+            headers={
+                "Authorization": "Basic " + base64.b64encode('{}:{}'.format(apikey_token,password).encode('utf-8')).decode('ascii')
+            }
+        )
+        print('Response HTTP Status Code: {status_code}'.format(
+            status_code=response.status_code))
+        print('Response HTTP Response Body: {content}'.format(
+            content=response.content))
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+
+
+send_request()
