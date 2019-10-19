cdee1ee88f0217fe63a45992ecaef0fceb9e3160
Brett Derham <brettderham@Robins-MacBook-Pro.local>
2019-10-18 18:35:52 -0700

    front end temp

--- ballpark-app/src/App.js
+++ ballpark-app/src/App.js
@@ -3,17 +3,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav'
 import { Navbar, Jumbotron, Button, Form, Col, Row } from 'react-bootstrap';
+
 // function App() {
 //   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
+//     <div classNameName="App">
+//       <header classNameName="App-header">
+//         <img src={logo} classNameName="App-logo" alt="logo" />
 //         <p>
 //           Edit <code>src/App.js</code> and save to reload.
 //         </p>
 //         <a
-//           className="App-link"
+//           classNameName="App-link"
 //           href="https://reactjs.org"
 //           target="_blank"
 //           rel="noopener noreferrer"
@@ -28,46 +30,109 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Jumbotron>
-            <h1>Ballpark Bookie</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
-            </p>
-            <p>
-             <Form>
-                <Form.Row>
-                  <Form.Group as={Col} md="6" controlId="team1">
-                    <Form.Label>Team One</Form.Label>
-                    <Form.Control type="team" placeholder="Enter Team One" />
-                  </Form.Group>
-                  <Form.Group as={Col} md="6" controlId="team2">                    
-                    <Form.Label>Team Two</Form.Label>
-                    <Form.Control type="team" placeholder="Enter Team Two" />
-                  </Form.Group>
-                  
-                </Form.Row>
-              </Form>
-            </p>
-
-            <p> 
-            <div class="mx-auto">
-              <Button variant="primary" type="compare">
-                Compare
-              </Button>
-            </div>
-            <div className="mx-auto">
-              <Form.Text className="text-muted">
-                Team ____ will win the game.
-              </Form.Text>
+        <Navbar bg="dark" variant="dark" sticky="top">
+          <Form inline style={{ padding: 10 }}>
+            <Navbar.Brand href="#home">Ballpark Bookie</Navbar.Brand>
+            <Button variant="danger">Home</Button>
+            <Button variant="primary">Big Predictions</Button>
+            <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+            <Button className=" "variant="danger">Search</Button>
+          </Form>
+        </Navbar>    
+        <div>
+          <Row>
+            <Col>
+              <div className="col-lg-2">
+                <div className="pl-3">
+                  <h2 className=" pl-3">Schedules</h2></div>
+                  <div onclick="window.location.href='https://www.nfl.com/'">
+                    <h5 className="text-dark"><u>Arizona Diamondbacks</u></h5></div>
+                  <div onclick="window.location.href='https://www.thefantasyfootballers.com'">
+                    <h5 className="text-dark"><u>Atlanta Braves</u></h5></div>
+                  <div onclick="window.location.href='https://www.fantasypros.com/'">
+                    <h5 className="text-dark"><u>Baltimore Orioles</u></h5></div>
+                  <div onclick="window.location.href='http://www.espn.com/nfl/'">
+                    <h5 className="text-dark"><u>Boston Red Sox</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Chicago White Sox</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Chicago Cubs</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Cincinnati Reds</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Cleveland Indians</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Colorado Rockies</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Detroit Tigers</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Houston Astros</u></h5></div>
+                  <div className="  " onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Kansas City Royals</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Los Angeles Angels</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Los Angeles Dodgers</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Miami Marlins</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Milwaukee Brewers</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Minnesota Twins</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>New York Yankees</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>New York Mets</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Oakland Athletics</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Philadelphia Phillies</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Pittsburgh Pirates</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>San Diego Padres</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>San Fransisco Giants</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Seattle Mariners</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>St. Louis Cardinals</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Tampa Bay Rays</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Texas Rangers</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Toronto Blue Jays</u></h5></div>
+                  <div onclick="window.location.href='https://www.ncaa.com/sports/football/fbs'">
+                    <h5 className="text-dark"><u>Washington Nationals</u></h5></div>  
               </div>
-            </p>
-        </Jumbotron>
+            </Col>
+            <Col md="auto">
+              <div className="col-lg-6">
+                <Row>
+                  <Form>
+                    <Form.Group as={Col} md="4" controlId="team1">
+                      <Form.Label>Team One</Form.Label>
+                      <Form.Control type="team" placeholder="Enter Team One" />
+                    </Form.Group>
+                    <Form.Group as={Col} md="4" controlId="team2">                    
+                      <Form.Label>Team Two</Form.Label>
+                      <Form.Control type="team" placeholder="Enter Team Two" />
+                    </Form.Group>
+                  </Form>
+                </Row>
+                <Row>
+                  <Button variant="primary" type="compare">
+                    Calculate
+                  </Button>
+                  <Form.Text classNameName="text-muted">
+                    Team ____ will win the game.
+                  </Form.Text>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
