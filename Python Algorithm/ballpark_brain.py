a9b2c71f3ea73b850c6d3e99994e70e0c658c0ff
kea <kea.braekman@mail.com>
2019-10-25 14:29:28 -0700

    JSON response to a dict

--- Python Algorithm/ballpark_brain.py
+++ Python Algorithm/ballpark_brain.py
@@ -1,40 +1,62 @@
-# Install the Python Requests library:
-# `pip install requests`
-
 import base64
 import requests
-
-season = '2018-2019-regular'
-apikey_token = 'e0c4e5ec-08d5-414d-88ce-9b392f'
-# apikey_token = 'Xx_thomas_xX'
-password = 'ilikevike'
-# MYSPORTSFEED = 0
-for_date = '20150606'
-# pull_url = 'https://api.mysportsfeeds.com/v1.0/pull/mlb/current_season.json?fordate=' + for_date
-pull_url = 'https://api.mysportsfeeds.com/v1.2/pull/mlb/2015-2016-regular/cumulative_player_stats.json'
-
-# pull_url = 'https://api.mysportsfeeds.com/v1.0/pull/mlb/2015-2016-regular/daily_player_stats.json?fordate=' + for_date
-
-
+import json
+#from ohmysportsfeedspy import MySportsFeeds
+#msf = MySportsFeeds(version="2.0")
+api_key = "e0c4e5ec-08d5-414d-88ce-9b392f"
+pswrd = "MYSPORTSFEEDS"
+#msf.authenticate(api_key, pswrd)
+#output = msf.msf_get_data(league='mlb',season='latest',feed='team_stats_totals',format='json')
+#print(output)
+#print("aids")
 def send_request():
-    # Request
+   # Request
+   try:
+       response = requests.get(
+           url="https://api.mysportsfeeds.com/v2.1/pull/mlb/latest/games.json",
+           params={
+               "fordate": "20161121"
+           },
+           headers={
+               "Authorization": "Basic " + base64.b64encode('{}:{}'.format(api_key,pswrd).encode('utf-8')).decode('ascii')
+           }
+       )
+       print('Response HTTP Status Code: {status_code}'.format(
+           status_code=response.status_code))
+       print('Response HTTP Response Body: {content}'.format(
+           content=response.content))
+   except requests.exceptions.RequestException:
+       print('HTTP Request failed')
 
+# send_request()
+
+
+
+
+def getData(teamA, teamB, keyword, season):
+    pull_url = 'https://api.mysportsfeeds.com/v2.1/pull/mlb/' + season + '-regular/games.json'
     try:
-        response = requests.get(
-            url=pull_url,
-            params={
-                "fordate": "20161121"
-            },
-            headers={
-                "Authorization": "Basic " + base64.b64encode('{}:{}'.format(apikey_token,password).encode('utf-8')).decode('ascii')
-            }
-        )
-        print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
+       response = requests.get(
+           url=pull_url,
+           params={
+               "fordate": "20161121"
+           },
+           headers={
+               "Authorization": "Basic " + base64.b64encode('{}:{}'.format(api_key,pswrd).encode('utf-8')).decode('ascii')
+           }
+       )
+    #    print('Response HTTP Status Code: {status_code}'.format(
+    #        status_code=response.status_code))
+    #    print('Response HTTP Response Body: {content}'.format(content=response.content))
+       datastore = json.loads(response.content)
+    #    print('datastore = ', datastore['games'][0]['schedule'])
+    #    print("YOYOYO ", type(datastore))
+    #    print('something', type((response.content)))
     except requests.exceptions.RequestException:
-        print('HTTP Request failed')
+       print('HTTP Request failed')
+    return(datastore)
 
 
-send_request()
+print(getData('Astros', 'Dodgers', 'games', '2019')['games'][0]['score'])
+
+# print(type(json.loads(getData('Astros', 'Dodgers', 'games', '2019'))))
