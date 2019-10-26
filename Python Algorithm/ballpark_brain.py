726ebec7052b10a07ba04023693d787079906931
kea <kea.braekman@mail.com>
2019-10-25 21:14:35 -0700

    collected home and away games (plus date & time) into two arrays

--- Python Algorithm/ballpark_brain.py
+++ Python Algorithm/ballpark_brain.py
@@ -1,66 +1,62 @@
 import base64
 import requests
 import json
-#from ohmysportsfeedspy import MySportsFeeds
-#msf = MySportsFeeds(version="2.0")
 api_key = "e0c4e5ec-08d5-414d-88ce-9b392f"
 pswrd = "MYSPORTSFEEDS"
-#msf.authenticate(api_key, pswrd)
-#output = msf.msf_get_data(league='mlb',season='latest',feed='team_stats_totals',format='json')
-#print(output)
-#print("aids")
-def send_request():
-   # Request
-   try:
-       response = requests.get(
-           url="https://api.mysportsfeeds.com/v2.1/pull/mlb/latest/games.json",
-           params={
-               "fordate": "20161121"
-           },
-           headers={
-               "Authorization": "Basic " + base64.b64encode('{}:{}'.format(api_key,pswrd).encode('utf-8')).decode('ascii')
-           }
-       )
-       print('Response HTTP Status Code: {status_code}'.format(
-           status_code=response.status_code))
-       print('Response HTTP Response Body: {content}'.format(
-           content=response.content))
-   except requests.exceptions.RequestException:
-       print('HTTP Request failed')
 
-# send_request()
-
-
-
-
-def getData(teamA, teamB, keyword, season):
-    pull_url = 'https://api.mysportsfeeds.com/v2.1/pull/mlb/' + season + '-regular/games.json'
+# Basic API Call with inputs as season and keyword to use in all data collection
+def basicAPICall(season, keyword):
     try:
-       response = requests.get(
-           url=pull_url,
-           params={
-               "fordate": "20161121"
-           },
-           headers={
-               "Authorization": "Basic " + base64.b64encode('{}:{}'.format(api_key,pswrd).encode('utf-8')).decode('ascii')
-           }
-       )
-    #    print('Response HTTP Status Code: {status_code}'.format(
-    #        status_code=response.status_code))
-    #    print('Response HTTP Response Body: {content}'.format(content=response.content))
-       datastore = json.loads(response.content)
+        response = requests.get(
+            url='https://api.mysportsfeeds.com/v2.1/pull/mlb/' + season + '-regular/' + keyword + '.json',
+            params={
+                "fordate": "20161121"
+            },
+            headers={
+                "Authorization": "Basic " + base64.b64encode('{}:{}'.format(api_key,pswrd).encode('utf-8')).decode('ascii')
+            }
+        )
     except requests.exceptions.RequestException:
-       print('HTTP Request failed')
-    return(datastore)
+        print('HTTP Request failed')
+    return (json.loads(response.content))
 
 
-print(len(getData('','', 'games', '2019')['games']))
+# Collects all home games of a team given a season and returns it as an array containing JSON Scores.
+def collectHomeGames(teamAbbreviation, season):
+    home = []
+    response = basicAPICall(season, 'games')
+    for i in range(len(response['games'])):
+        if(response['games'][i]['schedule']['homeTeam']['abbreviation'] == teamAbbreviation):
+            home.append(response['games'][i]['schedule']['startTime'])
+            home.append(response['games'][i]['score'])
+    return home
 
-for i in range(len(getData('','', 'games', '2019')['games'])):
-    if(getData('','', 'games', '2019')['games'][i]['schedule']['homeTeam']['id'] == 125):
-        print(getData('', '', 'games', '2019')['games'][i]['score'])
-    print('')
+def collectAllHomeGames(teamAbbreviation, season):
+    home = []
+    for i in range (2016, 2019):
+        home.append(collectHomeGames(teamAbbreviation, str(i)))
+    print(home)
 
-print(getData('Astros', 'Dodgers', 'games', '2019')['games'][0]['score'])
 
-# print(type(json.loads(getData('Astros', 'Dodgers', 'games', '2019'))))
+
+
+def collectAwayGames(teamAbbreviation, season):
+    away = []
+    response = basicAPICall(season, 'games')
+    for i in range(len(response['games'])):
+        if(response['games'][i]['schedule']['awayTeam']['abbreviation'] == teamAbbreviation):
+            away.append(response['games'][i]['schedule']['startTime'])
+            away.append(response['games'][i]['score'])
+    return away
+
+def collectAllHomeGames(teamAbbreviation, season):
+    away = []
+    for i in range (2016, 2019):
+        away.append(collectHomeGames(teamAbbreviation, str(i)))
+    print(away)
+
+
+
+
+collectAllHomeGames('OAK', '2016')
+
