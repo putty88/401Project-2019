c19a82486a6475442bc17e2d958a444957cdeac2
kea <kea.braekman@mail.com>
2019-10-30 17:55:46 -0700

    some csv development

---
+++ Python Algorithm/ballparkCSV.py
@@ -0,0 +1,35 @@
+import base64
+import requests
+import csv
+api_key = "e0c4e5ec-08d5-414d-88ce-9b392f"
+pswrd = "MYSPORTSFEEDS"
+
+
+def basicAPICall(season, keyword):
+    suffix = ''
+    if('gamelogs' in keyword):
+        suffix = '?team=det'
+    try:
+        response = requests.get(
+            url='https://api.mysportsfeeds.com/v2.1/pull/mlb/' + season + '-regular/' + keyword + '.csv' + suffix,
+            params={
+                "fordate": "20161121"
+            },
+            headers={
+                "Authorization": "Basic " + base64.b64encode('{}:{}'.format(api_key,pswrd).encode('utf-8')).decode('ascii')
+            }
+        )
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+    return (response.content)
+
+
+def collectHomeGames(season, teamAbbreviation):
+    with open(basicAPICall(season, 'games')) as csvfile:
+    # readCSV = csv.reader(csvfile, delimiter=','
+    
+
+
+
+
+
