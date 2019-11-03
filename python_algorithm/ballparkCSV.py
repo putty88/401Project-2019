9f993e52b390eaedc0e5bbd2714dda9e16a0c2ab
Riley Persily <persilyriley@yahoo.com>
2019-11-02 17:13:41 -0700

    Create function to select two teams and convert stats from CSV to NumPy Arrays

--- python_algorithm/ballparkCSV.py
+++ python_algorithm/ballparkCSV.py
@@ -2,6 +2,7 @@ import base64
 import json
 import requests
 import csv
+import pandas as pd
 api_key = "e0c4e5ec-08d5-414d-88ce-9b392f"
 pswrd = "MYSPORTSFEEDS"
 
@@ -27,15 +28,14 @@ def basicAPICall(season, keyword):
         print('HTTP Request failed')
     return (response.content)
 
-
-#def collectHomeGames(season, teamAbbreviation):
-#    with open(basicAPICall(season, 'games')) as csvfile:
-#    # readCSV = csv.reader(csvfile, delimiter=','
-    
-
-# def getSeasonalTeamStats(season, )
-
-
-
 keyword = 'team_stats_totals'
 basicAPICall('2018', keyword)
+
+def choose_teams(t1,t2):
+    df = pd.read_csv('teamStats.csv', index_col='#Team ID')
+    t1_stats = df.loc[t1].rename_axis('#Team ID').values
+    t2_stats = df.loc[t2].rename_axis('#Team ID').values
+    return t1_stats, t2_stats
+
+# Example of how to ask for NumPy Arrays of two chosen teams
+### print(choose_teams(111,133))
