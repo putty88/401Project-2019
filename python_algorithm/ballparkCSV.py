7c6a24af6caf1cd368b3cf8790031a54d655ec94
Riley Persily <persilyriley@yahoo.com>
2019-11-02 17:39:07 -0700

    Update naming of stored data from API; Collect player data

--- python_algorithm/ballparkCSV.py
+++ python_algorithm/ballparkCSV.py
@@ -21,21 +21,28 @@ def basicAPICall(season, keyword):
                 "Authorization": "Basic " + base64.b64encode('{}:{}'.format(api_key,pswrd).encode('utf-8')).decode('ascii')
             }
         )
-        f = open('teamStats.csv', "w")
+        f = open(keyword+'.csv', "w", encoding='utf-8')
         f.write(response.text)
         f.close()
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
     return (response.content)
 
-keyword = 'team_stats_totals'
-basicAPICall('2018', keyword)
+teams = 'team_stats_totals'
+players = 'player_stats_totals'
+basicAPICall('2018', teams)
+basicAPICall('2018', players)
 
 def choose_teams(t1,t2):
-    df = pd.read_csv('teamStats.csv', index_col='#Team ID')
+    df = pd.read_csv('team_stats_totals.csv', index_col='#Team ID')
     t1_stats = df.loc[t1].rename_axis('#Team ID').values
     t2_stats = df.loc[t2].rename_axis('#Team ID').values
+
+    ### Trying to collect player data
+    # df2 = pd.read_csv('player_stats_totals.csv', index_col='#Team ID')
+    # for x in len(df2.index):
+
     return t1_stats, t2_stats
 
 # Example of how to ask for NumPy Arrays of two chosen teams
-### print(choose_teams(111,133))
+print(choose_teams(111,133))
