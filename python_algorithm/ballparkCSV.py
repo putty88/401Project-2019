21cf7b90ca4fa4bf480254a29ed49372ea1ed1c3
kea <kea.braekman@mail.com>
2019-11-08 15:33:49 -0800

    changed some stuff

--- python_algorithm/ballparkCSV.py
+++ python_algorithm/ballparkCSV.py
@@ -21,21 +21,58 @@ def basicAPICall(season, keyword):
                 "Authorization": "Basic " + base64.b64encode('{}:{}'.format(api_key,pswrd).encode('utf-8')).decode('ascii')
             }
         )
-        f = open('teamStats.csv', "w")
+        f = open('teamStats' + season + '.csv', "w")
         f.write(response.text)
         f.close()
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
     return (response.content)
 
-keyword = 'team_stats_totals'
-basicAPICall('2018', keyword)
 
-def choose_teams(t1,t2):
-    df = pd.read_csv('teamStats.csv', index_col='#Team ID')
-    t1_stats = df.loc[t1].rename_axis('#Team ID').values
-    t2_stats = df.loc[t2].rename_axis('#Team ID').values
-    return t1_stats, t2_stats
+# basicAPICall('2016', 'team_stats_totals')
+# basicAPICall('2017', 'team_stats_totals')
+# basicAPICall('2018', 'team_stats_totals')
+# basicAPICall('2019', 'team_stats_totals')
+
+        
+
+
+
+
+def choose_team(A, season):
+    df = pd.read_csv('teamStats' + season + '.csv', index_col='#Team ID')
+    A_stats = df.loc[A].rename_axis('#Team ID').values
+    print(type(A_stats))
+    return A_stats
+
+choose_team(125, '2018')
+
+
+def normie(season):
+    data = pd.read_csv('TeamStats' + season + '.csv')
+    # print(data['#AtBats'].mean())
+    print(data)
+
+normie('2018')
+
+    # Loop thru all the stats
+    # Get the mean of each stat
+    # Get the std deviation of each stat
+    # Calculate : stats[i]-mean / std dev
+
+
+# def choose_team_B(B, season):
+#     df = pd.read_csv('teamStats' + season + '.csv', index_col='#Team ID')
+#     B_stats = df.loc[B].rename_axis('#Team ID').values
+#     # print(B_stats)
+#     return B_stats
+
+
+# def choose_teams(t1,t2):
+#     df = pd.read_csv('teamStats.csv', index_col='#Team ID')
+#     t1_stats = df.loc[t1].rename_axis('#Team ID').values
+#     t2_stats = df.loc[t2].rename_axis('#Team ID').values
+#     return t1_stats, t2_stats
 
 # Example of how to ask for NumPy Arrays of two chosen teams
 ### print(choose_teams(111,133))
