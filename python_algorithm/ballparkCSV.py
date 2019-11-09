34f3264e5624cf1dba0ed6f4f456c5ad46130d39
kea <kea.braekman@mail.com>
2019-11-08 16:53:35 -0800

    normalized and subtracted arrays

--- python_algorithm/ballparkCSV.py
+++ python_algorithm/ballparkCSV.py
@@ -3,6 +3,7 @@ import json
 import requests
 import csv
 import pandas as pd
+import numpy as np
 api_key = "e0c4e5ec-08d5-414d-88ce-9b392f"
 pswrd = "MYSPORTSFEEDS"
 
@@ -21,7 +22,7 @@ def basicAPICall(season, keyword):
                 "Authorization": "Basic " + base64.b64encode('{}:{}'.format(api_key,pswrd).encode('utf-8')).decode('ascii')
             }
         )
-        f = open(keyword+'.csv', "w", encoding='utf-8')
+        f = open(keyword + season + '.csv', "w", encoding='utf-8')
         f.write(response.text)
         f.close()
     except requests.exceptions.RequestException:
@@ -29,12 +30,9 @@ def basicAPICall(season, keyword):
     return (response.content)
 
 
-# basicAPICall('2016', 'team_stats_totals')
-# basicAPICall('2017', 'team_stats_totals')
-# basicAPICall('2018', 'team_stats_totals')
-# basicAPICall('2019', 'team_stats_totals')
 
-        
+
+# basicAPICall('2016', 'games')
 
 
 
@@ -42,37 +40,33 @@ def basicAPICall(season, keyword):
 def choose_team(A, season):
     df = pd.read_csv('teamStats' + season + '.csv', index_col='#Team ID')
     A_stats = df.loc[A].rename_axis('#Team ID').values
-    print(type(A_stats))
     return A_stats
 
-choose_team(125, '2018')
 
 
 headers = ['#GamesPlayed','#AtBats','#Runs','#Hits','#SecondBaseHits','#ThirdBaseHits','#Homeruns','#UnearnedRuns','#RunsBattedIn','#EarnedRuns','#BatterWalks','#BatterSwings','#BatterStrikes','#BatterStrikesFoul','#BatterStrikesMiss','#BatterStrikesLooking','#BatterGroundBalls','#BatterFlyBalls','#BatterLineDrives','#LeftOnBase','#BatterStrikeouts','#BatterSplitters','#BatterCutters','#Batter4SeamFastballs','#BatterSinkers','#BatterChangeups','#Batter2SeamFastballs','#OpponentsLeftOnBase','#BatterSliders','#BatterCurveballs','#StolenBases','#CaughtBaseSteals','#BatterStolenBasePct','#BattingAvg','#BatterOnBasePct','#BatterSluggingPct','#BatterOnBasePlusSluggingPct','#BatterIntentionalWalks','#HitByPitch','#BatterSacrificeBunts','#BatterSacrificeFlies','#TotalBases','#ExtraBaseHits','#BatterDoublePlays','#BatterTriplePlays','#BatterTagOuts','#BatterForceOuts','#BatterPutOuts','#BatterGroundOuts','#BatterFlyOuts','#BatterGroundOutToFlyOutRatio','#PitchesFaced','#PlateAppearances','#OpponentAtBats','#EarnedRunAvg','#InningsPitched','#HitsAllowed','#SecondBaseHitsAllowed','#ThirdBaseHitsAllowed','#RunsAllowed','#EarnedRunsAllowed','#HomerunsAllowed','#PitcherWalks','#PitcherSwings','#PitcherStrikes','#PitcherStrikesFoul','#PitcherStrikesMiss','#PitcherStrikesLooking','#PitcherGroundBalls','#PitcherFlyBalls','#PitcherLineDrives','#Pitcher4SeamFastballs','#PitcherSinkers','#PitcherChangeups','#Pitcher2SeamFastballs','#PitcherSliders','#PitcherSacrificeBunts','#PitcherCurveballs','#PitcherSplitters','#PitcherCutters','#PitcherSacrificeFlies','#PitcherStrikeouts','#PitchingAvg','#WalksAndHitsPerInningPitched','#Shutouts','#BattersHit','#PitcherIntentionalWalks','#PitcherGroundOuts','#PitcherFlyOuts','#PitcherWildPitches','#Balks','#PitcherStolenBasesAllowed','#PitcherCaughtStealing','#PickoffAttempts','#Pickoffs','#TotalBattersFaced','#PitchesThrown','#PitcherGroundOutToFlyOutRatio','#PitcherOnBasePct','#PitcherSluggingPct','#PitcherOnBasePlusSluggingPct','#StrikeoutsPer9Innings','#WalksAllowedPer9Innings','#HitsAllowedPer9Innings','#StrikeoutsToWalksRatio','#PitchesPerInning','#InningsPlayed','#TotalChances','#FielderForceOuts','#FielderTagOuts','#FielderPutOuts','#Assists','#Errors','#FielderDoublePlays','#FielderTriplePlays','#FielderStolenBasesAllowed','#FielderCaughtStealing','#FielderStolenBasePct','#PassedBalls','#FielderWildPitches','#FieldingPct','#DefenceEfficiencyRatio','#OutsFaced','#Wins','#Losses','#WinPct','#GamesBack','#RunsFor','#RunsAgainst','#RunDifferential']
 
-def normie(season):
+def normalize(teamStats, season):
     data = pd.read_csv('TeamStats' + season + '.csv')
-    # data[headers[]].mean()
-
-normie('2018')
-
-    # Loop thru all the stats
-    # Get the mean of each stat
-    # Get the std deviation of each stat
-    # Calculate : stats[i]-mean / std dev
-
-
-# def choose_team_B(B, season):
-#     df = pd.read_csv('teamStats' + season + '.csv', index_col='#Team ID')
-#     B_stats = df.loc[B].rename_axis('#Team ID').values
-#     # print(B_stats)
-#     return B_stats
+    means = []
+    stdevs = []
+    normal = []
+    for i in range(len(headers)):
+        means.append(data[headers[i]].mean())
+        stdevs.append(data[headers[i]].std())
+    for j in range(len(headers)):
+        normal.append( (teamStats[j+4] - means[j]) / stdevs[j])
+    return normal
 
 
-# def choose_teams(t1,t2):
-#     df = pd.read_csv('teamStats.csv', index_col='#Team ID')
-#     t1_stats = df.loc[t1].rename_axis('#Team ID').values
-#     t2_stats = df.loc[t2].rename_axis('#Team ID').values
-#     return t1_stats, t2_stats
+def subtractArrays(home, away):
+    delta = []
+    for i in range(len(home)):
+        delta.append(home[i]-away[i])
+    return delta
 
-# Example of how to ask for NumPy Arrays of two chosen teams
+
+# def createWinList(season):
+
+
+subtractArrays( normalize(choose_team(125, '2018'), '2018'),normalize(choose_team(136, '2018'), '2018'))
